8883f167e290808ef420fd5b8af9b7e60a148fd0
vaszol <vaszol@mail.ru>
2015-04-10 10:36:40 +0300

    порядок вызовов методов сортировка

--- src/TestCalc.java
+++ src/TestCalc.java
@@ -1,60 +1,69 @@
 
 // ≈ще одна "вкусность" Java 5 - можно импортировать static-методы
-import org.junit.Test;
-import org.junit.Before;
-import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
-import org.junit.Ignore;
-import org.junit.runner.JUnitCore;
+import java.util.Comparator;
 
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.runner.JUnitCore;
+
+import org.junit.runner.Description;
+import org.junit.runner.Request;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.Failure;
+
+
 public class TestCalc {
-    // @Test - это аннотаци€, котора€ обозначает, что метод должен быть вызван дл€ тестировани€ чего-нибудь
+    // ќбратите внимание на название - оно теперь сделано таким дл€ соблюдени€ пор€дка
 
     @Test
-    public void getSumTest() {
+    public void get001SumTest() {
         Calc c = new Calc();
         assertEquals(50, c.getSum(20, 30));
     }
 
+    // ќбратите внимание на название - оно теперь сделано таким дл€ соблюдени€ пор€дка
     @Test
-    public void getSubtractionTest() {
+    public void get002SubtractionTest() {
         Calc c = new Calc();
         assertEquals(-10, c.getSubtraction(20, 30));
     }
 
-    @BeforeClass
-    public static void allTestsStarted() {
-        System.out.println("All tests started");
-    }
+    // ћетод возвращает компаратор, который позвол€ет отсортировать методы в алфавитном пор€дке
+    private static Comparator forward() {
+        return new Comparator() {
 
-    @AfterClass
-    public static void allTestsFinished() {
-        System.out.println("All tests finished");
-    }
-
-    @Before
-    public void testStarted() {
-        System.out.println("Started");
-    }
-
-    @After
-    public void testFinished() {
-        System.out.println("Finished");
-    }
-
-    @Test
-    // ќбратите внимание аннотацию - она говорит, что тест будет проигнорирован. ≈сли ее убрать,
-    // то сообщение по€витьс€
-    @Ignore
-    public void testIgnored() {
-        System.out.println("Ignored test");
+            public int compare(Object o1, Object o2) {
+                Description d1 = (Description) o1;
+                Description d2 = (Description) o2;
+                return d1.getDisplayName().compareTo(d2.getDisplayName());
+            }
+        };
     }
 
     public static void main(String[] args) {
         JUnitCore core = new JUnitCore();
-        core.run(TestCalc.class);
+        core.addListener(new CalcListener());
+        // ќбратите внимание на этот вызов - ниже немного комментариев
+        core.run(Request.aClass(TestCalc.class).sortWith(forward()));
+    }
+}
+
+// Ќу здесь по идее должно быть все пон€тно
+class CalcListener extends RunListener {
+
+    @Override
+    public void testStarted(Description desc) {
+        System.out.println("Started:" + desc.getDisplayName());
+    }
+
+    @Override
+    public void testFinished(Description desc) {
+        System.out.println("Finished:" + desc.getDisplayName());
+    }
+
+    @Override
+    public void testFailure(Failure fail) {
+        System.out.println("Failed:" + fail.getDescription().getDisplayName() + " [" + fail.getMessage() + "]");
     }
 }
